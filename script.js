1bba459725cebba86f39efced653e97cdd5f7102
lekhachanh <lekhachanh123@gmail.com>
2019-06-19 08:34:20 +0700

    add file object

--- script.js
+++ script.js
@@ -13,23 +13,43 @@ function Hero(image, top, left, size){
       ' height="'+ this.size + '"' +
       ' src="' + this.image +'"' +
       ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
-  }
+  };
 
   this.moveRight = function(){
-    this.left += 20;
-    console.log('ok: ' + this.left);
+    this.left += 50;
+  };
+  this.moveLeft = function () {
+    this.left -= 50;
+  };
+  this.moveUp = function () {
+    this.top -= 50;
+  };
+  this.moveDown = function () {
+    this.top += 50;
   }
-
 }
 
-var hero = new Hero('pikachu.png', 20, 30, 200);
+let hero = new Hero('pikachu.png', 20, 30, 200);
 
 function start(){
   if(hero.left < window.innerWidth - hero.size){
-    hero.moveRight();
+    if(hero.top===20)
+      hero.moveRight();
+    }
+
+  else if (hero.top < window.innerHeight - hero.size) {
+    hero.moveDown();
+  }
+  if(hero.top >= window.innerHeight - hero.size&&hero.left>=80){
+    hero.moveLeft();
+  }
+  if(hero.top>20){
+    if(hero.left===80){
+      hero.moveUp();
+    }
   }
   document.getElementById('game').innerHTML = hero.getHeroElement();
-  setTimeout(start, 500)
+  setTimeout(start, 100);
 }
 
 start();
